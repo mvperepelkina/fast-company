2cb09445c0916aa32b6137d30f9e4ee013edf795
mvperepelkina <marvladperepelkina@gmail.com>
2023-01-16 22:57:46 +0300

    feat: added task solution

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const BookMark = ({ status, toggleBookmark, userId }) => {
     return (
         <button
             className="btn btn-outline-none"
-            onClick={() => toggleBookmark(userId)}>
+            onClick={() => toggleBookmark(userId)}
+        >
             {status ? (
                 <i className="bi bi-bookmark-star-fill"></i>
             ) : (
@@ -12,6 +14,12 @@ const BookMark = ({ status, toggleBookmark, userId }) => {
             )}
         </button>
     );
+};
+
+BookMark.propTypes = {
+    status: PropTypes.bool,
+    toggleBookmark: PropTypes.func,
+    userId: PropTypes.string
 };
 
 export default BookMark;
